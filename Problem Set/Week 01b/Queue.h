4ccae1da1d3cbecbfb5045822d0cf114e6c766aa
brmuch <br20130906@gmail.com>
2018-11-30 10:39:14 +1100

    Create Queue.h

---
+++ Problem Set/Week 01b/Queue.h
@@ -0,0 +1,5 @@
+// Integer Queue ADO header file
+void QueueInit();        // set up empty queue
+int  QueueIsEmpty();     // check whether queue is empty
+void QueueEnqueue(int);  // insert int at end of queue
+int  QueueDequeue();     // remove int from front of queue
