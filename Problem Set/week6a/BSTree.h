2e405b5cef994b1207e500fe759f652f9eba6919
RB <br20130906@gmail.com>
2019-01-31 17:39:38 +1100

    Create 1

---
+++ Problem Set/week6a/BSTree.h
@@ -0,0 +1,24 @@
+// Binary Search Tree ADT interface ... COMP9024 17s2
+
+#include <stdbool.h>
+
+typedef int Item;      // item is just a key
+
+typedef struct Node *Tree;
+
+Tree newTree();        // create an empty Tree
+void freeTree(Tree);   // free memory associated with Tree
+void showTree(Tree);   // display a Tree (sideways)
+
+bool TreeSearch(Tree, Item);   // check whether an item is in a Tree
+int  TreeHeight(Tree);         // compute height of Tree
+int  TreeNumNodes(Tree);       // count #nodes in Tree
+Tree TreeInsert(Tree, Item);   // insert a new item into a Tree
+Tree TreeDelete(Tree, Item);   // delete an item from a Tree
+
+// internal functions made visible for testing
+Tree rotateRight(Tree);
+Tree rotateLeft(Tree);
+Tree insertAtRoot(Tree, Item);
+Tree partition(Tree, int);
+Tree rebalance(Tree);
