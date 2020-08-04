d96e64dcea592195d876a14725f9290d932c8b12
br20130906@gmail.com <br20130906@gmail.com>
2020-08-04 20:55:35 +1000

    upload set

--- Algorithms/DP/BackPack/Readme.txt
+++ Algorithms/DP/BackPack/Readme.txt
@@ -1,5 +1,6 @@
 LintCode # 92 BackPack problem
 
-ViolentSol                      22% pass
-TopToBottomRem       		44% pass
-TwoDimensionArray     		78% pass
+ViolentSol                      22%  pass
+TopToBottomRem       		44%  pass
+TwoDimensionArray     		78%  pass
+Set                             78%  pass
---
+++ Algorithms/DP/BackPack/Set.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @return: The maximum size
+    """
+    def backPack(self, m, A):
+        # write your code here
+        possibleVLS = {0}
+    
+        for item in A:
+            temp = list(possibleVLS)
+            possibleVLS = possibleVLS.union(set([i + item for i in temp]))
+    
+        return max([i for i in list(possibleVLS) if i <= m])
